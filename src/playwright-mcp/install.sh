84897022dec54e5d4827ea8a6fb61c895b860377
Ryan Arora <arora.ryan@gmail.com>
2025-03-09 01:31:09 -0500

    try again 14

--- src/playwright-mcp/install.sh
+++ src/playwright-mcp/install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 set -e
-
 npx playwright install --with-deps
-mv /root/.cache/ms-playwright/ /home/vscode/.cache/ms-playwright/
-chown -R vscode:vscode /home/vscode/.cache/ms-playwright/
