cf1f2702ea873a997bb8525576dd8e1b890ca5ef
Ryan Arora <arora.ryan@gmail.com>
2025-03-09 01:16:54 -0500

    try again

--- src/playwright-mcp/install.sh
+++ src/playwright-mcp/install.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
 set -e
-
-# Determine the appropriate non-root user
-# I stole this from desktop-lite
-# https://github.com/devcontainers/features/blob/main/src/desktop-lite/install.sh
-USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-            USERNAME=${CURRENT_USER}
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=root
-    fi
-elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
-    USERNAME=root
-fi
-
-# Change to non-root user
-sudo su ${USERNAME}
-
-# Install Playwright
-npx playwright install --with-deps
+sudo -u vscode npx playwright install --with-deps
